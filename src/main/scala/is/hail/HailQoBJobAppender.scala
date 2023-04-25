7f35297f512af675244701cae6cb00f00c463634
Dan King <dking@broadinstitute.org>
2023-04-25 12:36:25 -0400

    literally wtf

---
+++ src/main/scala/is/hail/HailQoBJobAppender.scala
@@ -0,0 +1,59 @@
+package is.hail
+
+import java.io._
+// import org.apache.log4j._
+// import org.apache.log4j.spi._
+import org.apache.logging.log4j.core._
+import org.apache.logging.log4j.core.layout._
+import org.apache.logging.log4j.core.appender._
+import org.apache.logging.log4j.core.config.plugins._
+
+object HailQoBJobAppender {
+  @PluginFactory
+  def createAppender(
+    @PluginAttribute("name") name: String,
+    @PluginAttribute("ignoreExceptions") ignoreExceptions: Boolean,
+    @PluginElement("Layout") layout: Layout[_],
+    @PluginElement("Filters") filter: Filter
+  ): HailQoBJobAppender = {
+    return new HailQoBJobAppender()
+  }
+
+  private var theOneHailQoBJobAppender: HailQoBJobAppender = null
+
+  def getTheOneHailQoBJobAppender: HailQoBJobAppender = theOneHailQoBJobAppender
+}
+
+@Plugin(name = "HailQoBJobAppender", category = "Core", elementType = "appender", printObject = true)
+class HailQoBJobAppender extends AbstractAppender(
+  "HailQoBJobAppender", null, PatternLayout.createDefaultLayout(), false, null
+) {
+  private[this] val logFormat: String = "%d{yyyy-MM-dd HH:mm:ss.SSS} %c{1}: %p: %m%n"
+  private[this] val layout = PatternLayout.createDefaultLayout()
+  private[this] var fio = new PrintWriter(new BufferedOutputStream(new FileOutputStream("/tmp/foo")))
+  System.err.println("I am alive")
+
+  if (theOneHailQoBJobAppender != null) {
+    throw new RuntimeException("absolutely not.")
+  }
+
+  theOneHailQoBJobAppender = this
+
+  override def append(event: LogEvent): Unit = {
+    val s = layout.toSerializable(event)
+    System.err.println("append " + s)
+    fio.write(s)
+  }
+
+  def close() = {
+    fio.flush()
+    fio.close()
+  }
+
+  def requiresLayout(): Boolean = false
+
+  def changeFile(file: String): Unit = {
+    System.err.println("changeFile")
+    fio = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))
+  }
+}
--- src/main/scala/is/hail/Test.scala
+++ src/main/scala/is/hail/Test.scala
@@ -1,23 +1,18 @@
 package is.hail
 
 import org.apache.log4j._
+import collection.JavaConverters._
 
 object Test {
   private val log = Logger.getLogger(getClass.getName())
-  private[this] val logFormat: String = "%d{yyyy-MM-dd HH:mm:ss.SSS} %c{1}: %p: %m%n"
 
   def main(args: Array[String]): Unit = {
     log.error("ONE")
-
-    LogManager.getRootLogger().removeAllAppenders()
-    val fa = new FileAppender()
-    fa.setName("Hail File Appender: ")
-    fa.setFile("/tmp/abc")
-    fa.setLayout(new PatternLayout(logFormat))
-    fa.setThreshold(Level.INFO)
-    fa.activateOptions()
-    LogManager.getRootLogger().addAppender(fa)
-
+    HailQoBJobAppender.getTheOneHailQoBJobAppender().changeFile("file2")
     log.error("TWO")
+    HailQoBJobAppender.getTheOneHailQoBJobAppender().changeFile("file3")
+    log.error("THREE")
+    HailQoBJobAppender.getTheOneHailQoBJobAppender().changeFile("file4")
+    LogManager.getRootLogger().error("FOUR")
   }
 }
